4f04db2c5450ee424287fd0506f8151e9fda36d7
ajvidal98 <alvarovdl4@gmail.com>
2017-11-12 18:42:34 -0300

    colocando sout

--- proyecto/src/main/Main.java
+++ proyecto/src/main/Main.java
@@ -27,23 +27,29 @@ public class Main {
             System.out.println("-------------------------------------------");
 
             if (opc.equalsIgnoreCase("A")) {
+                System.out.println("-------------------------------");
                 System.out.print("Ingrese las coordenadas de A: ");
+                System.out.println("-------------------------------");
 
                 String crdA = leer.nextLine();
                 String[] lugarA = crdA.split(",");
 
                 int crdAx = Integer.parseInt(lugarA[0]);
                 int crdAy = Integer.parseInt(lugarA[1]);
-
+                
+                System.out.println("--------------------------------");
                 System.out.print("Ingrese las coordenadas de B: ");
+                System.out.println("--------------------------------");
 
                 String crdB = leer.nextLine();
                 String[] lugarB = crdB.split(",");
 
                 int crdBx = Integer.parseInt(lugarB[0]);
                 int crdBy = Integer.parseInt(lugarB[1]);
-
+                
+                System.out.println("---------------------------------");
                 System.out.print("Ingrese las coordenadas de C: ");
+                System.out.println("---------------------------------");
 
                 String crdC = leer.nextLine();
                 String[] lugarC = crdC.split(",");
@@ -54,31 +60,39 @@ public class Main {
             } else {
 
                 if (opc.equalsIgnoreCase("B")) {
+                      
                     System.out.print("Ingrese las coordenadas de A: ");
+                    System.out.println("-------------------------------");
 
                     String crdA = leer.nextLine();
                     String[] lugarA = crdA.split(",");
 
                     int crdAx = Integer.parseInt(lugarA[0]);
                     int crdAy = Integer.parseInt(lugarA[1]);
-
+                    
+                    System.out.println("-------------------------------");
                     System.out.print("Ingrese las coordenadas de B: ");
+                    System.out.println("-------------------------------");
 
                     String crdB = leer.nextLine();
                     String[] lugarB = crdB.split(",");
 
                     int crdBx = Integer.parseInt(lugarB[0]);
                     int crdBy = Integer.parseInt(lugarB[1]);
-
+                    
+                    System.out.println("------------------------------");
                     System.out.print("Ingrese las coordenadas de C: ");
+                    System.out.println("-------------------------------");
 
                     String crdC = leer.nextLine();
                     String[] lugarC = crdC.split(",");
 
                     int crdCx = Integer.parseInt(lugarC[0]);
                     int crdCy = Integer.parseInt(lugarC[1]);
-
+                    
+                    System.out.println("------------------------------");
                     System.out.print("Ingrese las coordenadas de D: ");
+                    System.out.println("------------------------------");
 
                     String crdD = leer.nextLine();
                     String[] lugarD = crdD.split(",");
